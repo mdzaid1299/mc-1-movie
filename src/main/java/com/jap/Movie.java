6dfb8ca22645ea34b33d896006e87664be6226ad
HP <mdzaid1299@gmail.com>
2022-09-08 10:10:47 +0530

    ini

--- src/main/java/com/jap/Movie.java
+++ src/main/java/com/jap/Movie.java
@@ -1,16 +1,35 @@
 package com.jap;
 
 public class Movie {
-    private int movieId;
-    private String movieName;
-    private String genre;
-    private String releaseDate;
+    private final int movieId;
+    private final String movieName;
+    private final String genre;
+    private final String releaseDate;
 
     // create getter and setter for all the above attributes
 
 
     //complete the parameterized constructor
     public Movie(int movieId, String movieName, String genre, String releaseDate) {
+        this.movieId = movieId;
+        this.movieName = movieName;
+        this.genre = genre;
+        this.releaseDate = releaseDate;
+    }
 
+    public int getMovieId() {
+        return movieId;
+    }
+
+    public String getMovieName() {
+        return movieName;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
     }
 }
